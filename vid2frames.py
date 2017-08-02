58755ff8a12543efffce507906ce4b8727aabc21
Andy Zhang <jz359@cornell.edu>
2017-08-02 15:19:45 -0400

    shell script works; should probably modify to output to a designated output folder

--- vid2frames.py
+++ vid2frames.py
@@ -1,5 +1,5 @@
 import cv2
-video_capture = cv2.VideoCapture("sample.mp4")
+video_capture = cv2.VideoCapture("../videos/sample.mp4")
 
 count=0
 
@@ -7,7 +7,7 @@ while True:
     # get frame by frame
     ret, frame = video_capture.read()
     if ret is True:
-        cv2.imwrite('image'+str(count)+'.jpg',frame)
+        cv2.imwrite('./frames/image'+str(count)+'.jpg',frame)
         count+=1
     else:
         break
