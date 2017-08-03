e7ce3f9f4ba9d44356772b5ca6f1a2e001d688d2
Andy Zhang <jz359@cornell.edu>
2017-08-03 15:48:47 -0400

    fixed frames2vid

--- frames2vid.py
+++ frames2vid.py
@@ -2,8 +2,10 @@ import cv2
 import numpy as np
 import os
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc, 30.0, (704,400))
+fourcc = cv2.cv.FOURCC(*'XVID')
+#for later cv2 versions
+#fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 30.0, (704,400))
 
 PATHS = [ os.path.join('./output/', 'frame{}.jpg'.format(i)) for i in range(0, 3382) ]
 
