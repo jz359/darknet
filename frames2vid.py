7cb2e8a1613570c222e3aa16d3638420e8a00e06
Andy Zhang <jz359@cornell.edu>
2017-08-03 11:12:54 -0400

    modified video conversion code

--- frames2vid.py
+++ frames2vid.py
@@ -5,7 +5,7 @@ import os
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi',fourcc, 30.0, (704,400))
 
-PATHS = [ os.path.join('./test_frames/', 'frame{}.jpg'.format(i)) for i in range(0, 3382) ]
+PATHS = [ os.path.join('./output/', 'frame{}.jpg'.format(i)) for i in range(0, 3382) ]
 
 
 for frame in PATHS:
