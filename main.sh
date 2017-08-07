23f3cd725364d477976dee2b525ae66bb2c647bc
Andy Zhang <jz359@cornell.edu>
2017-08-07 10:26:31 -0400

    shell script now modified to be run on the cluster

--- main.sh
+++ main.sh
@@ -1,14 +1,24 @@
 # to run on an image
 # ./darknet detect cfg/yolo.cfg yolo.weights data/dog.jpg
 
-FRAMES=./frames/*
+DATAPATH=/data/yolo_movies/
 count=0
-for f in $FRAMES
+for f in $DATAPATH
 do
-	echo "processing $f file"
-	./darknet detect cfg/yolo.cfg yolo.weights $f
-	rename="frame"
-	rename="$rename$count"
-	count=$((count+1))
-	mv "./predictions.png" "./$rename.png"
+	echo "processing $f video into frames"
+	python vid2frames.py $f
+	echo "done processing; frames should be in $f/"
+	echo "running darknet on each frame"
+
+	FRAMEPATH=$f/
+	echo "the path we're writing frames to $FRAMEPATH"
+
+	for img in $FRAMEPATH
+	do
+		./darknet detect cfg/yolo.cfg yolo.weights $img
+		rename="output"
+		rename="$rename$count"
+		count=$((count+1))
+		mv "./predictions.png" "$f/$rename.png"
+	done
 done
