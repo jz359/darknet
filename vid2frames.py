c22f9ed77287ef91447d90cd7972d2dedf75c907
Andy Zhang <jz359@cornell.edu>
2017-08-07 10:27:33 -0400

    shell script now modified to be run on the cluster

--- vid2frames.py
+++ vid2frames.py
@@ -1,14 +1,36 @@
 import cv2
-video_capture = cv2.VideoCapture("../videos/sample.mp4")
+import argparse
+import os
 
-count=0
+def main(args):
+	video_capture = cv2.VideoCapture(args.input_video)
+	framepath = os.path.basename(args.input_video)
+	name, ext = framepath.split('.')
+	dir_name = name
 
-while True:
-    # get frame by frame
-    ret, frame = video_capture.read()
-    if ret is True:
-        cv2.imwrite('./frames/image'+str(count)+'.jpg',frame)
-        count+=1
-    else:
-        break
-video_capture.release()
+	if not os.path.exists('/data/yolo_movies/'+dir_name+'/'):
+		os.makedirs('/data/yolo_movies/'+dir_name+'/')
+
+	framepath = '/data/yolo_movies/'+dir_name+'/'
+
+	count=0
+
+	while True:
+	    # get frame by frame
+	    ret, frame = video_capture.read()
+	    if ret is True:
+	        cv2.imwrite(framepath+'image'+str(count)+'.jpg',frame)
+	        count+=1
+	    else:
+	        break
+	video_capture.release()
+
+
+def parse_arguments(argv):
+	parser = argparse.ArgumentParser()
+	parser.add_argument('input_video', type=str, help='Target video to process into frames.')
+
+	return parser.parse_args(argv)
+
+if __name__ == '__main__':
+	main(parse_arguments(sys.argv[1:]))
